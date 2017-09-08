4e70e7de33b0916d15d70b4a4d7d72fb18b32fd9
李飞飞 <lifeifeixz@sina.cn>
2017-09-08 21:16:40 +0800

    初次提交

---
+++ src/components/alert/alert.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="alert">
+		这里是弹出信息
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"alert"
+	}
+</script>
+
+<style>
+	.alert{
+		width: 20%;
+		height: 120px;
+		border:1px solid red;
+		background: red;
+		position: fixed;
+		border-radius: 5px;
+		box-shadow:#ccc -1px -1px 2px 1px;
+		top: 100px;
+		left: 40%;
+		z-index: 9999;
+	}
+</style>
