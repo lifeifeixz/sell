3950c72400522cf95e06d55ccee8676116a76672
李飞飞 <lifeifeixz@sina.cn>
2017-09-08 21:16:40 +0800

    初次提交

---
+++ src/components/foot.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="foot">
+		<a href="">中国</a> |
+		<a href="">上海</a> |
+		<a href="">天气预报</a>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "foot"
+	}
+</script>
+
+<style>
+	.foot {
+		font-size: 12px;
+		font-family: "华文行楷";
+	}
+</style>
